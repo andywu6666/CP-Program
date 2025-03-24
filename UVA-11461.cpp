ccfd79a0879fae06167b9d72d1d11c643eaf1aaa
andywu6666 <andywu0010@gmail.com>
2025-03-24 23:10:39 +0800

    facing float problem

--- UVA-11461.cpp
+++ UVA-11461.cpp
@@ -7,9 +7,11 @@ using namespace std;
 int main(){
 int squareCount = 0;
 int a, b;
-while(cin >> a >> b && (a != 0 && b != 0)){
+
+while(cin >> a >> b && !(a == 0 && b == 0)){
     for (int i = a ; i <= b; i++){
-        if ( (i / sqrt(i) ) == 0 )
+        int root = static_cast<int>(sqrt(i));
+        if (root * root == i)
         squareCount++;
     }
 
