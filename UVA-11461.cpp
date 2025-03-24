a27cfaa10f439f3104c79ddb36c65ed92a7e9e82
andywu6666 <andywu0010@gmail.com>
2025-03-24 23:12:34 +0800

    AIver.

--- UVA-11461.cpp
+++ UVA-11461.cpp
@@ -2,23 +2,17 @@
 #include <cmath>
 using namespace std;
 
-
-
-int main(){
-int squareCount = 0;
-int a, b;
-
-while(cin >> a >> b && !(a == 0 && b == 0)){
-    for (int i = a ; i <= b; i++){
-        int root = static_cast<int>(sqrt(i));
-        if (root * root == i)
-        squareCount++;
+int main() {
+    int a, b;
+    while (cin >> a >> b && !(a == 0 && b == 0)) {  // 正確終止條件
+        int cnt = 0;
+        for (int i = a; i <= b; ++i) {
+            int root = static_cast<int>(sqrt(i));
+            if (root * root == i) {  // 關鍵判斷式
+                ++cnt;
+            }
+        }
+        cout << cnt << endl;
     }
-
-
-
-}
-cout << squareCount << endl;
-
-
+    return 0;
 }
