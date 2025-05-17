2413ae1e8d73e0114efede29c61a118ddb0617a0
andywu6666 <andywu0010@gmail.com>
2025-05-17 10:16:10 +0800

    F_1133317-11000.cpp

--- 1133317-11000.cpp
+++ 1133317-11000.cpp
@@ -5,9 +5,9 @@ using namespace std;
 int main()
 {
 	long long N, total = 0;
-	long long male = 1, female = 0;
+	long long male = 0, normalFemale = 0;
 	vector<long long> male_arr;
-	vector<long long> female_arr;
+	vector<long long> normalFemale_arr;
 	//initially one female bee, after N years, that would be total amount of bees.
 	while (true) //one female bee give birth to one male bee
 	{
@@ -15,21 +15,25 @@ int main()
 		if (N == -1)
 			break;
 
-		//first year
+		if (N == 0){
+			cout << 0 << " " << 1 << endl;
+			continue;
+		}
+
+
 		
-			male = 1;
-			female = 0;
-			total = 2;
+			male = 0;
+			normalFemale = 0;
 			male_arr.push_back(male);
-			female_arr.push_back(female);
+			normalFemale_arr.push_back(normalFemale);
 		
-			for (int i = 1; i < N; i ++){
-			male = male_arr.back() + female_arr.back() + 1;
-			female += 1;
+			for (int i = 1; i <= N; i ++){
+			male = male_arr.back() + normalFemale_arr.back() + 1;
+			normalFemale = male_arr.back();
 			male_arr.push_back(male);
-			female_arr.push_back(female);
-			total = male + female + 1;
+			normalFemale_arr.push_back(normalFemale);
 			}
+			total = male + normalFemale + 1;
 			
 		cout << male << " " << total << endl;
 
