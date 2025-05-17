d63108fd64b082303688c3ee512214dbe1dfbec4
andywu6666 <andywu0010@gmail.com>
2025-05-17 11:49:16 +0800

    Update AtCoder 318 - B. Overlapping sheets.cpp

--- AtCoder 318 - B. Overlapping sheets.cpp
+++ AtCoder 318 - B. Overlapping sheets.cpp
@@ -1,12 +1,33 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main()
 {
+    vector<vector<int> > is_covered;
+    int N, A, B, C, D;
+    cin >> N;
+    for (int Case = 0; Case < N; Case++)
+    {
+    cin >> A >> B >> C >> D;
+    
 
+    int x = A > B ? A - B : B - A; 
+    int y = C > D ? C - D : D - C;
 
+    for(int i = 0; i < x; i++){
+        for(int j = 0; j < y; j++){
+            is_covered[i][j].push_back(1);
 
+            if (is_covered[i][j] == 1){ //already covered
+            continue;
+            }
+    
+        }
+    }
 
+    }
+        cout << is_covered.size() << endl;
 
     return 0;
 }
