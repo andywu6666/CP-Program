9a7aed65282b5a98ba4613de940e3b00a51c8655
andywu6666 <andywu0010@gmail.com>
2025-05-30 16:55:22 +0800

    Revised UVA-10642

--- Lab II/1133317-10642.cpp
+++ Lab II/1133317-10642.cpp
@@ -1,10 +1,11 @@
+//Accepted
 #include <iostream>
 #include <vector>
 using namespace std;
 
 int main()
 {
-	int C = 0;
+	long long int C = 0; // modify to long long
 	cin >> C;
 	for (int i = 0; i < C; i++)
 	{
