ffdca54037dbf5ff3679cd389a389e35a88f1152
andywu6666 <andywu0010@gmail.com>
2025-05-31 20:32:37 +0800

    Update 1133317-10035.cpp

--- Lab II/1133317-10035.cpp
+++ Lab II/1133317-10035.cpp
@@ -1,3 +1,4 @@
+//Accepted
 #include <iostream>
 #include <vector>
 using namespace std;
@@ -5,17 +6,17 @@ using namespace std;
 int main()
 {
     // two unsigned less than 10 digits -> using unsigned long long
-    unsigned long long a , b;
+    unsigned long long a, b;
     while (cin >> a >> b && (a != 0 || b != 0))
     {
         int sum;
         int carry = 0, carryCount = 0;
         while (a > 0 || b > 0 || carry > 0)
         {
-            sum = (a % 10) + (b % 10) + carry ;
-             a /= 10;
+            sum = (a % 10) + (b % 10) + carry;
+            a /= 10;
             b /= 10;
-        
+
             if (sum >= 10)
             {
                 carry = 1;
@@ -25,21 +26,19 @@ int main()
             {
                 carry = 0;
             }
-       
         }
-        switch(carryCount)
+        switch (carryCount)
         {
-            case 0:
+        case 0:
             cout << "No carry operation." << endl;
             break;
-            case 1:
+        case 1:
             cout << "1 carry operation." << endl;
             break;
-            default:
+        default:
             cout << carryCount << " carry operations." << endl;
             break;
         }
-        
     }
 
     return 0;
