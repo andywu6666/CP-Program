c788b9ad37b1d6521a3ec15ee56cce717314b0fa
andywu6666 <andywu0010@gmail.com>
2025-08-11 21:30:33 +0800

    Update 10922 2 the 9s.cpp

--- CPE-1star/10922 2 the 9s.cpp
+++ CPE-1star/10922 2 the 9s.cpp
@@ -3,56 +3,46 @@
 #include <vector>
 using namespace std;
 
-int main ()
+int main()
 {
     string N;
 
-while (cin >> N && N != "0")
-{
-    string result = N;
-    bool flag = false;
-    int count = 1;
+    while (cin >> N && N != "0")
+    {
+        string result = N;
+        bool flag = false;
+        int count = 1;
     // store numbers up to 1000 digits
     recursive:
-    int i = result.length() - 1;
-    int sum = 0;
-    int digit = 0;
-    while (i >=0)
-    {
-
-         digit = (i >= 0) ? result[i--] - '0' : 0;
-        sum += digit;
-       
-
-    }
-
-     if (sum % 9 == 0)
-    {
-        if (sum >= 10)
+        int i = result.length() - 1;
+        int sum = 0;
+        int digit = 0;
+        while (i >= 0)
         {
-            count++;
-            result = to_string(sum);
-            goto recursive;
+
+            digit = (i >= 0) ? result[i--] - '0' : 0;
+            sum += digit;
         }
+
+        if (sum % 9 == 0)
+        {
+            if (sum >= 10)
+            {
+                count++;
+                result = to_string(sum);
+                goto recursive;
+            }
+            else
+            {
+                flag = true;
+            }
+        }
+
+        if (flag)
+            cout << N << " is a multiple of 9 and has 9-degree " << count << "." << endl;
         else
-        {
-            flag = true;
-        }
+            cout << N << " is not a multiple of 9." << endl;
     }
-
-     if (flag)
-        cout << N << " is a multiple of 9 and has 9-degree " << count << "." << endl;
-     else
-        cout << N << " is not a multiple of 9." << endl;
-    
-
-}
-
-
-
-
-
-
 
     return 0;
 }
