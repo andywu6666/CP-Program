0233c9dd4f7b42579b4fd237688b408edf1e5ecc
andywu6666 <andywu0010@gmail.com>
2025-08-19 13:57:29 +0800

    Accepted UVA-12019

---
+++ CPE-1star/12019 Doom’s Day Algorithm.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string days[8] = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+const int dooms[] = {0, 10, 21, 7, 4, 9, 6, 11, 8, 5, 10, 7, 12};
+
+int main()
+{
+int t;
+cin >> t;
+while (t--)
+{
+    int M, D;
+    cin >> M >> D;
+    int dayIndex = 0;
+    int difference = D - dooms[M];
+    if (difference < 0)
+    {
+        dayIndex = (difference % 7 + 7) % 7;
+    }
+    else
+    {
+        dayIndex = difference % 7;
+    }
+    cout << days[dayIndex] << endl;
+
+
+}
+
+
+
+    return 0;
+}
