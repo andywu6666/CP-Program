358c4bb839b6861698d1ef8f3589e2fc9e807223
andywu6666 <andywu0010@gmail.com>
2025-11-21 21:54:05 +0800

    Create 10041 2 and revised 10190

--- CPE-1star/10190 Divide But Not Quite Conquer.cpp
+++ CPE-1star/10190 Divide But Not Quite Conquer.cpp
@@ -2,34 +2,31 @@
 #include <vector>
 using namespace std;
 
-int main()
-{
-    long long int m, n;
-    while (cin >> m >> n)
-    {
-        bool flag = true;
-        vector<long long int> vec;
-        if (m <= 2 || n < 1) flag = false;
-        vec.push_back(m);
-        while (m % n == 0 && n > 1)
-        {
-                m /= n;
-                vec.push_back(m);
+int main() {
+    long long n, m;
+    while (cin >> n >> m) {
+        if (m <= 1 || n < 1) { // m <= 1 不行, n <= 0 不行
+            cout << "Boring!" << endl;
+            continue;
         }
-        
-        if (flag)
-        {
-            for (int i = 0; i < vec.size(); i++)
-            {
-                cout << vec[i];
-                if (i != vec.size() - 1)
-                    cout << " ";
+
+        vector<long long> seq;
+        seq.push_back(n);
+
+        while (n > 1 && n % m == 0) {
+            n /= m;
+            seq.push_back(n);
+        }
+
+        if (n == 1) {
+            for (int i = 0; i < seq.size(); i++) {
+                if (i) cout << " ";
+                cout << seq[i];
             }
+            cout << endl;
+        } else {
+            cout << "Boring!" << endl;
         }
-        else
-        {
-            cout << "Boring!";
-        }
-        cout << endl;
     }
+    return 0;
 }
